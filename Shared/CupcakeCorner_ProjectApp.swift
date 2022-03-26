65d69d24973e3ad484fdc7706f809119f9b138ea
arjundoel <arjundoel@gmail.com>
2022-03-26 18:30:17 +0000

    Initial Commit

---
+++ Shared/CupcakeCorner_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_ProjectApp.swift
+//  Shared
+//
+//  Created by Arjun Doel on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
